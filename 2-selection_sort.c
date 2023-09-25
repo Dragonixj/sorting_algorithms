8c4755ac57f2991e3538f500f72feda60d28084c
Dragonixj <delafiado@gmail.com>
2023-09-25 17:04:30 +0000

    Task 2: initial commit

---
+++ 2-selection_sort.c
@@ -0,0 +1,6 @@
+#include "sort.h"
+/**
+ * selection_sort - sorts and array of integers in ascending order
+ * @array: the pointer to the first element of the array
+ * @size: the size of the array
+ */
