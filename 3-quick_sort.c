27a12e2a13ba12d44446de23d2756259874a2e67
Dragonixj <delafiado@gmail.com>
2023-09-26 11:15:38 +0000

    Task 3: wrong output

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -22,12 +22,14 @@ int lomuto_part(int *array, int l_idx, int h_idx, size_t size)
             tmp = array[i];
             array[i] = array[j];
             array[j] = tmp;
+            print_array(array, size); /* print after each swap*/
         }
     }
 
     tmp = array[i + 1];
     array[i + 1] = array[h_idx];
     array[h_idx] = tmp;
+    print_array(array, size);
 
     return (i + 1);
 }
@@ -66,5 +68,5 @@ void quick_sort(int *array, size_t size)
     if (array == NULL || size < 2)
         return;
 
-    quick_sort_rc(array, 0, size - 1,size);
+    quick_sort_rc(array, 0, size - 1, size);
 }
