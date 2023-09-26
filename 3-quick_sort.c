e21205dc822784492563f4281be2db8aadc4d9f4
Dragonixj <delafiado@gmail.com>
2023-09-26 11:09:03 +0000

    Task 3: compilation issues

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -31,3 +31,40 @@ int lomuto_part(int *array, int l_idx, int h_idx, size_t size)
 
     return (i + 1);
 }
+
+
+/**
+ * quick_sort_rc - Recursive function for quick_sort
+ * @array: the array to be sorted
+ * @l_idx: the low index of the partition
+ * @h_idx: the high index of the partition
+ * @size: the size of the array
+ */
+void quick_sort_rc(int *array, int l_idx, int h_idx, size_t size)
+{
+    int pivot_idx;
+
+    if(l_idx < h_idx)
+    {
+        pivot_idx = lomuto_part(array, l_idx, h_idx, size);
+        print_array(array, size);
+
+        quick_sort_rc(array, l_idx, pivot_idx - 1, size);
+        quick_sort_rc(array, pivot_idx + 1, h_idx, size);
+    }
+}
+
+
+/**
+ * quick_sort - Sorts an array of integers in ascending order using quick sort
+ *              algorithm(Lomuto partition scheme)
+  * @array: the array to be sorted
+  * @size: the size of the array
+  */
+void quick_sort(int *array, size_t size)
+{
+    if (array == NULL || size < 2)
+        return;
+
+    quick_sort_rc(array, 0, size - 1,size);
+}
