86b862ee598f77975178ab876e4b80bf2d5884a6
Dragonixj <delafiado@gmail.com>
2023-09-26 10:41:23 +0000

    Task 2: output issue fixed( > instead of < in nested for

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -19,7 +19,7 @@ void selection_sort(int *array, size_t size)
          * of array*/
         for (j = i + 1; j < size; j++)
         {
-            if (array[j] < array[min_index])
+            if (array[j] < array[min_index]) /* issue here* fixed*/
                 min_index = j;
         }
 
