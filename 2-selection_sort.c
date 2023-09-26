808430e06df38e7b86f56d6de81aa26cb4eee1b6
Dragonixj <delafiado@gmail.com>
2023-09-26 10:30:22 +0000

    Task 2: wrong expected output after compilation

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -4,3 +4,33 @@
  * @array: the pointer to the first element of the array
  * @size: the size of the array
  */
+
+void selection_sort(int *array, size_t size)
+{
+    size_t i, j;
+    size_t min_index;
+    int tmp;
+
+    for (i = 0; i < size -1; i++)
+    {
+        min_index = i;
+
+        /* Find the index of the min element in the unsorted portion
+         * of array*/
+        for (j = i + 1; j < size; j++)
+        {
+            if (array[j] > array[min_index])
+                min_index = j;
+        }
+
+        /* swap the min element with the first element */
+        if (min_index != i)
+        {
+            tmp = array[i];
+            array[i] = array[min_index];
+            array[min_index] = tmp;
+            print_array(array, size); /* printing the array after each swap*/
+
+        }
+    }
+}
